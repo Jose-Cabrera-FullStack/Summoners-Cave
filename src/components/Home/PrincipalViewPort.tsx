623ee6739aa0fddaceea8ffccd95c0c9ab0bd807
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2020-10-19 23:20:11 -0300

    se esta por cambiar las rutas

--- src/components/Home/PrincipalViewPort.tsx
+++ src/components/Home/PrincipalViewPort.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Div, Text } from "./style";
-// import Ekko from "../../static/img/Ekko.png";
+
+const Ekko = require("../../static/img/Ekko.png");
 
 const PrincipalViewPort = () => {
   return (
     <>
       <Div>
         <Text theme={{ color: "red" }}>Prueba de contingencia</Text>
-        <img src={"Ekko"} alt="" />
+        <img src={Ekko} alt="" />
       </Div>
     </>
   );
